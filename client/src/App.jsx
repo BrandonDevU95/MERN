8f1ab45bc8ecb1ab7eb62b16ab72210a47b922eb
Brandon <brandon.vargas1095@hotmail.com>
2020-06-04 01:45:40 -0500

    Adding sass to project Video 35

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Web Personal</h1>
+      <h2>Web personal</h2>
+    </div>
+  );
+}
+
+export default App;
