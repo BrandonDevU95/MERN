2823ccd1b76488c805a7879f0d929b16f1284102
Brandon <brandon.vargas1095@hotmail.com>
2020-06-08 21:20:26 -0500

    Added library Ant-Design to project Video 36

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import { DatePicker } from 'antd';
 import './App.scss';
 
 function App() {
@@ -7,8 +7,13 @@ function App() {
     <div className="app">
       <h1>Web Personal</h1>
       <h2>Web personal</h2>
+      <DatePicker onChange={onChange} />
     </div>
   );
 }
 
 export default App;
+
+function onChange(date, dateString) {
+  console.log(date, dateString);
+}
