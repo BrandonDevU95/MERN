e33da567d7688d7de0fde210dd0cb1b28a7fd9f8
Brandon <brandon.vargas1095@hotmail.com>
2020-06-13 19:43:15 -0500

    Route System for Layout Admin Video 49

--- client/src/Layout/_Admin.jsx
+++ client/src/Layout/_Admin.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import { Layout } from 'antd';
+import {Route} from 'react-router-dom'
+import './_Admin.scss'
 
-const _Admin = () => {
+const _Admin = (props) => {
+
+	const {routes} = props
+	const {Header, Content, Footer} = Layout;
+
+
 	return (
 		<Layout>
 			<h2>Menu Slider Admin</h2>
-			<div>Contenido...</div>
-			<h5>Footer...</h5>
+			<Layout>
+				<Header>Header...</Header>
+				<Content>
+					<LoadRoutes routes={routes}/>
+				</Content>
+				<Footer>Brandon Vargas </Footer> 
+			</Layout>
 		</Layout>
 	);
 };
+
+function LoadRoutes ({routes}) {
+
+	return routes.map((route, index) => (
+		<Route key={index} path={route.path} exact={route.exact} component={route.component} />
+	))
+}
 
 export default _Admin;
