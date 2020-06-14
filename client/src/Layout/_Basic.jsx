1bd9e338fbadc0ab060a60131f4f3fa5ed99355b
Brandon <brandon.vargas1095@hotmail.com>
2020-06-13 20:06:15 -0500

    Route System for Layout Basic Video 51

--- client/src/Layout/_Basic.jsx
+++ client/src/Layout/_Basic.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { Layout } from 'antd';
 
-const _Basic = () => {
+import './_Basic.scss';
+
+const _Basic = (props) => {
+	const { routes } = props;
+	const { Content, Footer } = Layout;
+
 	return (
-		<div>
-			<h2>Menu Slider Basic</h2>
-		</div>
-	)
+		<Layout>
+			<h2>Menu</h2>
+			<Layout>
+				<Content>
+					<LoadRoutes routes={routes} />
+				</Content>
+				<Footer>Brandon Vargas</Footer>
+			</Layout>
+		</Layout>
+	);
+};
+
+function LoadRoutes({ routes }) {
+	return routes.map((route, index) => (
+		<Route
+			key={index}
+			path={route.path}
+			exact={route.exact}
+			component={route.component}
+		/>
+	)); 
 }
 
-export default _Basic
+export default _Basic;
