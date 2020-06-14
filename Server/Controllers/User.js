fc779f4c9d7281fe6790a3961be87fbc5864661e
Brandon <brandon.vargas1095@hotmail.com>
2020-06-14 01:23:39 -0500

    Endpoint to Create New User Video 60

--- Server/Controllers/User.js
+++ Server/Controllers/User.js
@@ -2,7 +2,24 @@ const bcrypt = require('bcrypt-nodejs')
 const User = require('../Models/User')
 
 function signUp (req, res) {
-	console.log('Endping de SignUp')
+	const user = new User()
+	const {email, password, repeatPassword} = req.body;
+	user.email = email
+	user.role = 'admin'
+	user.active = false
+
+	if(!password || !repeatPassword) {
+		res.status(404).send({message: 'Las contraseñas son obligatorias'})
+	} else {
+		if(password !== repeatPassword) {
+			res.status(404).send({message: 'Las contraseñas no son iguales'})
+		} else {
+			bcrypt.hash(password, null, null, function(error, hash) {
+				
+			})
+			res.status(200).send({message: 'Todo OK'})
+		}
+	}
 }
 
 module.exports = {signUp}
