dc71c40df3af3deca77f596acc1eb482303f0e14
Brandon <brandon.vargas1095@hotmail.com>
2020-06-15 01:56:28 -0500

    Saving the Form Information Video 66

--- client/src/Components/Admin/RegisterForm/RegisterForm.jsx
+++ client/src/Components/Admin/RegisterForm/RegisterForm.jsx
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Input, Button, Checkbox, notification } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import './RegisterForm.scss';
 
 const RegisterForm = () => {
+	const [inputs, setInputs] = useState({
+		email: '',
+		password: '',
+		repeatPassword: '',
+		privacyPolicy: false,
+	});
+
+	const changeForm = (e) => {
+		if (e.target.name === 'privacyPolicy') {
+			setInputs({
+				...inputs,
+				[e.target.name]: e.target.checked,
+			});
+		} else {
+			setInputs({
+				...inputs,
+				[e.target.name]: e.target.value,
+			});
+		}
+	};
+
+	const register = e => {
+		// e.preventDefault();
+		console.log(inputs);
+	};
+
 	return (
-		<Form className='register-form'>
+		<Form className='register-form' onFinish={register} onChange={changeForm}>
 			<Form.Item>
 				<Input
 					type='email'
 					name='email'
 					placeholder='Correo electronico'
 					className='register-form__input'
-					prefix={<UserOutlined style={{ color: 'rgba(0,0,0,.25)' }} className='icon-input' />}
+					prefix={
+						<UserOutlined
+							style={{ color: 'rgba(0,0,0,.25)' }}
+							className='icon-input'
+						/>
+					}
+					value={inputs.email}
 				/>
 			</Form.Item>
 			<Form.Item>
@@ -21,7 +53,13 @@ const RegisterForm = () => {
 					name='password'
 					placeholder='Contraseña'
 					className='register-form__input'
-					prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} className='icon-input' />}
+					prefix={
+						<LockOutlined
+							style={{ color: 'rgba(0,0,0,.25)' }}
+							className='icon-input'
+						/>
+					}
+					value={inputs.password}
 				/>
 			</Form.Item>
 			<Form.Item>
@@ -30,11 +68,17 @@ const RegisterForm = () => {
 					name='repeatPassword'
 					placeholder='Repetir Contraseña'
 					className='register-form__input'
-					prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} className='icon-input' />}
+					prefix={
+						<LockOutlined
+							style={{ color: 'rgba(0,0,0,.25)' }}
+							className='icon-input'
+						/>
+					}
+					value={inputs.repeatPassword}
 				/>
 			</Form.Item>
 			<Form.Item>
-				<Checkbox name='privacyPolicy' >
+				<Checkbox name='privacyPolicy' checked={inputs.privacyPolicy}>
 					He leido y acepto la politica de privacidad.
 				</Checkbox>
 			</Form.Item>
