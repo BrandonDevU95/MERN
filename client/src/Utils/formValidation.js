cfaa94bbfa5985480d26fbe9fe529f5e9a0d79da
Brandon <brandon.vargas1095@hotmail.com>
2020-06-15 14:44:24 -0500

    Creating Reusable Validation Functions Video 67

---
+++ client/src/Utils/formValidation.js
@@ -0,0 +1,33 @@
+export function minLengtValidation(inputData, minLength) {
+	const { value } = inputData;
+	removeClassErrorSucccess(inputData);
+
+	if (value.lenght >= minLength) {
+		inputData.classList.add('success');
+		return true;
+	} else {
+		inputData.classList.add('error');
+		return false;
+	}
+}
+
+export function emailValidation(inputData) {
+	const emailValid = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	const { value } = inputData;
+
+	removeClassErrorSucccess(inputData);
+
+	const resultValidation = emailValid.test(value);
+	if (resultValidation) {
+		inputData.classList.add('success');
+		return true;
+	} else {
+		inputData.classList.add('error');
+		return false;
+	}
+}
+
+function removeClassErrorSucccess(inputData) {
+	inputData.classList.remove('success');
+	inputData.classList.remove('error');
+}
