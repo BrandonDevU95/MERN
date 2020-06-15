1e099ec8ff350bec910813969ff621b67d1f581a
Brandon <brandon.vargas1095@hotmail.com>
2020-06-15 00:05:29 -0500

    SignIn Basic Structure Video 63

--- client/src/Pages/Admin/SignIn/SignIn.jsx
+++ client/src/Pages/Admin/SignIn/SignIn.jsx
@@ -7,14 +7,24 @@ import './SignIn.scss'
 const SignIn = () => {
 
    const {Content} = Layout;
-   const {TabPanel} = Tabs;
+   const {TabPane} = Tabs;
 
    return (
-      <Layout>
+      <Layout className='sign-in'>
          <Content className='sign-in__content'>
             <h1 className='sign-in__content-logo'>
-               <img src={Logo} alt="Brandon Vargas" />
+               <img src={Logo} alt="Brandon Vargas"/>
             </h1>
+				<div className="sign-in__content-tabs">
+					<Tabs type='card'>
+						<TabPane tab={<span>Entrar</span>} key='1'>
+							Componente Login
+						</TabPane>
+						<TabPane tab={<span>Nuevo Usuario</span>} key='2' >
+							Componenete Register Form
+						</TabPane>
+					</Tabs>
+				</div>
          </Content>
       </Layout>
    )
