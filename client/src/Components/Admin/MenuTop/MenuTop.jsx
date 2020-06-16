2b33ce6ffe88cf572ee2270ccf8eaa6337067007
Brandon <brandon.vargas1095@hotmail.com>
2020-06-16 02:00:32 -0500

    Adding Functionality to the Logout Button Video 88

--- client/src/Components/Admin/MenuTop/MenuTop.jsx
+++ client/src/Components/Admin/MenuTop/MenuTop.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './MenuTop.scss';
 import { Button } from 'antd';
-import { MenuUnfoldOutlined, PoweroffOutlined, MenuFoldOutlined } from '@ant-design/icons';
+import {
+	MenuUnfoldOutlined,
+	PoweroffOutlined,
+	MenuFoldOutlined,
+} from '@ant-design/icons';
 import Logo from '../../../Assets/img/png/muvalogo_clean.png';
+import { logout } from '../../../API/auth';
 
 const MenuTop = (props) => {
 	const { menuCollapsed, setMenuCollapsed } = props;
+	const logoutUser = () => {
+		logout();
+		window.location.reload();
+	};
 
 	return (
 		<div className='menu-top'>
@@ -23,7 +32,7 @@ const MenuTop = (props) => {
 				</Button>
 			</div>
 			<div className='menu-top__right'>
-				<Button type='link'>
+				<Button type='link' onClick={logoutUser}>
 					<PoweroffOutlined />
 				</Button>
 			</div>
