fd9172312286fcda1020edba30df90733332adf5
Brandon <brandon.vargas1095@hotmail.com>
2020-06-15 20:36:21 -0500

    Service to Create Tokens Video 74

---
+++ Server/Services/jwt.js
@@ -0,0 +1,30 @@
+const jwt = require('jwt-simple');
+const moment = require('moment');
+
+const SECRET_KEY = 'WmufO6lfMA8vfYyRtTM6AZodtyMCnass2slYO8brKdtwNWGrQq';
+
+exports.createAccesstoken = function (user) {
+	const payload = {
+		id: user._id,
+		name: user.name,
+		lastname: user.lastname,
+		email: user.email,
+		rol: user.rol,
+		createToken: moment().unix(),
+		exp: moment().add(3, 'hours').unix(),
+	};
+
+	return jwt.encode(payload, SECRET_KEY);
+};
+
+exports.createRefreshToken = function (user) {
+	const payload = {
+		id: user._id,
+		exp: moment().add(30, 'days').unix(),
+	};
+	return jwt.encode(payload, SECRET_KEY);
+};
+
+exports.decodedToken = function (token) {
+	return jwt.decode(token, SECRET_KEY, true);
+};
