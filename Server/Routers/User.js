46ce02e9551240a534db216e3e5d65d189f9276e
Brandon <brandon.vargas1095@hotmail.com>
2020-06-17 13:58:01 -0500

    Middleware to Block Users Video 93

--- Server/Routers/User.js
+++ Server/Routers/User.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const UserController = require('../Controllers/User');
+const md_auth = require('../Middleware/authenticated');
 
 const api = express.Router();
 
 api.post('/sign-up', UserController.signUp);
 api.post('/signIn', UserController.signIn);
-api.get('/users', UserController.getUsers);
+api.get('/users', [md_auth.ensureAuth], UserController.getUsers);
 
 module.exports = api;
