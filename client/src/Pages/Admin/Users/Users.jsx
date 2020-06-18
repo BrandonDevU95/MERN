aaf47708e79f8ce8fe1e5230c50f4a2de68214ed
Brandon <brandon.vargas1095@hotmail.com>
2020-06-17 21:12:39 -0500

    Run Endpoint to get Users Video 95

--- client/src/Pages/Admin/Users/Users.jsx
+++ client/src/Pages/Admin/Users/Users.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getAccessTokenApi } from '../../../API/auth';
+import { getUsersApi } from '../../../API/user';
 import './Users.scss';
 
 const Users = () => {
+	const [users, setUsers] = useState([]);
+	const token = getAccessTokenApi();
+
+	useEffect(() => {
+		getUsersApi(token).then((response) => {
+			setUsers(response);
+		});
+	}, [token]);
 	return (
 		<div>
 			<h1>Estamos en Users ...</h1>
