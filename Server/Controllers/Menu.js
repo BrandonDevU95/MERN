ead5a0adb5740f1918a1209342c0d95686f93f5c
Brandon <brandon.vargas1095@hotmail.com>
2020-06-24 19:29:28 -0500

    Endpoint to Create New Menus Video 126

--- Server/Controllers/Menu.js
+++ Server/Controllers/Menu.js
@@ -1,7 +1,25 @@
 const Menu = require('../Models/Menu');
 
 function addMenu(req, res) {
-	console.log('Add menu');
+	const { title, url, order, active } = req.body;
+	const menu = new Menu();
+
+	menu.title = title;
+	menu.url = url;
+	menu.order = order;
+	menu.active = active;
+
+	menu.save((err, createMenu) => {
+		if (err) {
+			res.status(500).send({ message: 'Error del servidor' });
+		} else {
+			if (!createMenu) {
+				res.status(404).send({ message: 'Error al crear el menu' });
+			} else {
+				res.status(200).send({ message: 'Menu Creado' });
+			}
+		}
+	});
 }
 
 module.exports = {
