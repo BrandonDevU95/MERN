5f9053cad4e5933497faae91c50add1e1fa10b44
Brandon <brandon.vargas1095@hotmail.com>
2020-06-25 00:30:09 -0500

    Listing Menus with Drag and Drop Video 130 2/2

--- client/src/Components/Admin/MenuWeb/MenuWebList/MenuWebList.jsx
+++ client/src/Components/Admin/MenuWeb/MenuWebList/MenuWebList.jsx
@@ -1,31 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { Switch, List, Button, Modal as ModalAntd, notification } from 'antd';
-import {} from '@ant-design/icons';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 import Modal from '../../../Modal';
 import DragSortableList from 'react-drag-sortable';
 
-import './MenuWebList';
+import './MenuWebList.scss';
 
 const { confirm } = ModalAntd;
 
 const MenuWebList = (props) => {
 	const { menu, setReloadMenuWeb } = props;
+	const [listItems, setListItems] = useState([]);
 	const [isVisibleModal, setIsVisibleModal] = useState(false);
 	const [modalTitle, setModalTitle] = useState('');
+	const [modalContent, setModalContent] = useState(null);
 
 	useEffect(() => {
 		const listItemsArray = [];
 
 		menu.forEach((item) => {
 			listItemsArray.push({
-				content: (
-					<div>
-						<p>{listItemsArray.title}</p>
-					</div>
-				),
+				content: <MenuItem item={item} />,
 			});
 		});
-		setList;
+		setListItems(listItemsArray);
 	}, [menu]);
 
 	const onSort = (sortedList, dropEvent) => {};
@@ -36,10 +34,34 @@ const MenuWebList = (props) => {
 				<Button type='primary'>Menu</Button>
 			</div>
 			<div className='menu-web-list__items'>
-				<DragSortableList />
+				<DragSortableList
+					items={listItems}
+					onSort={onSort}
+					type='vertical'
+				/>
 			</div>
 		</div>
 	);
 };
 
 export default MenuWebList;
+
+function MenuItem(props) {
+	const { item } = props;
+
+	return (
+		<List.Item
+			actions={[
+				<Switch defaultChecked={true} />,
+				<Button type='primary'>
+					<EditOutlined />
+				</Button>,
+				<Button type='danger'>
+					<DeleteOutlined />
+				</Button>,
+			]}
+		>
+			<List.Item.Meta title={item.title} description={item.url} />
+		</List.Item>
+	);
+}
