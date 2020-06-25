9d9def9cb0fd55056e43419b0b3769a1b146b265
Brandon <brandon.vargas1095@hotmail.com>
2020-06-24 22:08:57 -0500

    Listing Menus with Drag and Drop Video 129 1/2

--- client/src/Components/Admin/MenuWeb/MenuWebList/MenuWebList.jsx
+++ client/src/Components/Admin/MenuWeb/MenuWebList/MenuWebList.jsx
@@ -1,14 +1,43 @@
 import React, { useState, useEffect } from 'react';
+import { Switch, List, Button, Modal as ModalAntd, notification } from 'antd';
+import {} from '@ant-design/icons';
+import Modal from '../../../Modal';
+import DragSortableList from 'react-drag-sortable';
+
+import './MenuWebList';
+
+const { confirm } = ModalAntd;
 
 const MenuWebList = (props) => {
 	const { menu, setReloadMenuWeb } = props;
+	const [isVisibleModal, setIsVisibleModal] = useState(false);
+	const [modalTitle, setModalTitle] = useState('');
+
+	useEffect(() => {
+		const listItemsArray = [];
+
+		menu.forEach((item) => {
+			listItemsArray.push({
+				content: (
+					<div>
+						<p>{listItemsArray.title}</p>
+					</div>
+				),
+			});
+		});
+		setList;
+	}, [menu]);
+
+	const onSort = (sortedList, dropEvent) => {};
 
 	return (
-		<div>
-			<h1>Menu Web List...</h1>
-			{menu.map((item) => (
-				<p key={item._id}>{item.title}</p>
-			))}
+		<div className='menu-web-list'>
+			<div className='menu-web-list__header'>
+				<Button type='primary'>Menu</Button>
+			</div>
+			<div className='menu-web-list__items'>
+				<DragSortableList />
+			</div>
 		</div>
 	);
 };
