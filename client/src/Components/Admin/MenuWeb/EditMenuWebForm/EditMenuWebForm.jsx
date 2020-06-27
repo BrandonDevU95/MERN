00b9c6290d756fe281dc42924540e21e3514934d
BrandonU <brandon.vargas1095@hotmail.com>
2020-06-26 23:25:00 -0500

    Logic to Update Menu Edit Video 138

--- client/src/Components/Admin/MenuWeb/EditMenuWebForm/EditMenuWebForm.jsx
+++ client/src/Components/Admin/MenuWeb/EditMenuWebForm/EditMenuWebForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Input, Button, notification } from 'antd';
 import { LinkOutlined, FontSizeOutlined } from '@ant-design/icons';
 import { updateMenuApi } from '../../../../API/menu';
@@ -8,9 +8,43 @@ import './EditMenuWebForm.scss';
 
 const EditMenuWebForm = (props) => {
 	const { setIsVisibleModal, setReloadMenuWeb, menu } = props;
+	const [menuWebData, setMenuWebData] = useState(menu);
+
+	useEffect(() => {
+		setMenuWebData(menu);
+	}, [menu]);
+
+	const editMenu = (event) => {
+		if (!menuWebData.title || !menuWebData.url) {
+			notification['error']({
+				message: 'Todos los campos son obligatorios',
+			});
+		} else {
+			const accesToken = getAccessTokenApi();
+
+			updateMenuApi(accesToken, menuWebData._id, menuWebData)
+				.then((response) => {
+					notification['success']({
+						message: response,
+					});
+					setIsVisibleModal(false);
+					setReloadMenuWeb(true);
+				})
+				.catch(() => {
+					notification['error']({
+						message: 'Error del servidor',
+					});
+				});
+		}
+	};
+
 	return (
 		<div className='edit-menu-web-form'>
-			<EditForm />
+			<EditForm
+				menuWebData={menuWebData}
+				setMenuWebData={setMenuWebData}
+				editMenu={editMenu}
+			/>
 		</div>
 	);
 };
@@ -18,20 +52,29 @@ const EditMenuWebForm = (props) => {
 export default EditMenuWebForm;
 
 function EditForm(props) {
-	const { menuWebData, setMenuWebData, editMenu, menu } = props;
+	const { menuWebData, setMenuWebData, editMenu } = props;
 
 	return (
-		<Form className='form-edit'>
+		<Form className='form-edit' onFinish={editMenu}>
 			<Form.Item>
 				<Input
-					prefix={<FontSizeOutlined className='icon-input' />}
+					prefix={<FontSizeOutlined />}
+					className='icon-input'
 					placeholder='Titulo'
+					value={menuWebData.title}
+					onChange={(e) =>
+						setMenuWebData({ ...menuWebData, title: e.target.value })
+					}
 				/>
 			</Form.Item>
 			<Form.Item>
 				<Input
 					prefix={<LinkOutlined className='icon-input' />}
 					placeholder='URL'
+					value={menuWebData.url}
+					onChange={(e) =>
+						setMenuWebData({ ...menuWebData, url: e.target.value })
+					}
 				/>
 			</Form.Item>
 			<Form.Item>
