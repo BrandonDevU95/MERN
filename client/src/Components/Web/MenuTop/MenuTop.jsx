3ffabdd192ee1cd1e59bc31772d7e6879ab326c3
BrandonU <brandon.vargas1095@hotmail.com>
2020-06-27 00:57:28 -0500

    Adding the Logo to the Menu Video 142

--- client/src/Components/Web/MenuTop/MenuTop.jsx
+++ client/src/Components/Web/MenuTop/MenuTop.jsx
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { Menu } from 'antd';
 import { Link } from 'react-router-dom';
+import Logo from '../../../Assets/img/png/muvalogo_clean.png';
 
 import './MenuTop.scss';
 
 const MenuTop = () => {
 	return (
 		<Menu className='menu-top' mode='horizontal'>
-			<Menu.Item className='menu-top__logo'>Logo...</Menu.Item>
+			<Menu.Item className='menu-top__logo'>
+				<Link to='/'>
+					<img src={Logo} alt='MUVA WEB DESIGN' />
+				</Link>
+			</Menu.Item>
 			<Menu.Item className='menu-top__item'>
 				<Link to={'/'}>Inicio</Link>
 			</Menu.Item>
