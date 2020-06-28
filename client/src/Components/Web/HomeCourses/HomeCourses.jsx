3c044f08fb1acad758ecb3f7a2091b3eed8d3fab
BrandonU <brandon.vargas1095@hotmail.com>
2020-06-28 00:34:56 -0500

    Component to Show Courses VIdeo 147 2/3

--- client/src/Components/Web/HomeCourses/HomeCourses.jsx
+++ client/src/Components/Web/HomeCourses/HomeCourses.jsx
@@ -17,10 +17,82 @@ const HomeCourses = () => {
 				<h2>Aprende y mejora tus habilidades</h2>
 			</Col>
 			<Col lg={4} />
-			<Col lg={16}></Col>
+			<Col lg={16}>
+				<Row className='row-courses'>
+					<Col md={6}>
+						<CardCourse
+							image={reactJsHooks}
+							title='React JS Hooks'
+							subtitle='Intermedio Reatc/Java'
+							link='#'
+						/>
+					</Col>
+					<Col md={6}>
+						<CardCourse
+							image={rectNative}
+							title='React Native Expo'
+							subtitle='Intermedio Reatc/Java'
+							link='#'
+						/>
+					</Col>
+					<Col md={6}>
+						<CardCourse
+							image={javaScript}
+							title='JavaScript ES6'
+							subtitle='Basico JavaScript'
+							link='#'
+						/>
+					</Col>
+					<Col md={6}>
+						<CardCourse
+							image={wordPress}
+							title='WordPress'
+							subtitle='Basico WordPress'
+							link='#'
+						/>
+					</Col>
+				</Row>
+				<Row className='row-courses'>
+					<Col md={6}>
+						<CardCourse
+							image={prestaShop}
+							title='PrestaShop'
+							subtitle='Basico PrestaShop'
+							link='#'
+						/>
+					</Col>
+					<Col md={6} />
+					<Col md={6} />
+					<Col md={6}>
+						<CardCourse
+							image={cssGrid}
+							title='CSS Grid'
+							subtitle='Intermedio CSS'
+							link='#'
+						/>
+					</Col>
+				</Row>
+			</Col>
 			<Col lg={4} />
 		</Row>
 	);
 };
 
 export default HomeCourses;
+
+function CardCourse(props) {
+	const { image, title, subtitle, link } = props;
+	const { Meta } = Card;
+
+	return (
+		<a href={link} target='_blank' rel='noopener noreferrer'>
+			<Card
+				className='home-courses__card'
+				cover={<img src={image} alt={title} />}
+				actions={[<Button>INGESAR</Button>]}
+			>
+				<Meta title={title} description={subtitle} />
+			</Card>
+		</a>
+	);
+}
