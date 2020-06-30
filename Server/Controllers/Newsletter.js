193e139ae33b67d6f7b3df23630897e713f500fe
Brandon <brandon.vargas1095@hotmail.com>
2020-06-30 01:06:50 -0500

    Endpoint to Save Emails Video 157

--- Server/Controllers/Newsletter.js
+++ Server/Controllers/Newsletter.js
@@ -1,7 +1,28 @@
 const Newsletter = require('../Models/Newsletter');
 
 function suscribeEmail(req, res) {
-	console.log('Correo OK...');
+	const email = req.params.email;
+	const newsletter = new Newsletter();
+
+	if (!email) {
+		res.status(404).send({ code: 404, message: 'El email es obligatorio' });
+	} else {
+		newsletter.email = email.toLowerCase();
+		newsletter.save((err, newsletterStored) => {
+			if (err) {
+				res.status(500).send({ code: 500, message: 'El correo ya existe' });
+			} else {
+				if (!newsletterStored) {
+					res.status(400).send({
+						code: 400,
+						message: 'Error al registrar correo',
+					});
+				} else {
+					res.status(200).send({ code: 200, message: 'Email registrado' });
+				}
+			}
+		});
+	}
 }
 
 module.exports = {
