56d785c0615983a955af64b06be0993281d5e3ca
Brandon <brandon.vargas1095@hotmail.com>
2020-06-30 14:44:46 -0500

    Obtaining the Courses from the Database Video 166

---
+++ client/src/Components/Admin/Courses/CoursesList/CoursesList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './CoursesList.scss';
+
+const CoursesList = (props) => {
+	const { courses, setReloadCourses } = props;
+	console.log(props);
+	return (
+		<div>
+			<h1>Lorem ipsum dolor sit amet.</h1>
+		</div>
+	);
+};
+
+export default CoursesList;
--- client/src/Pages/Admin/Courses.jsx
+++ client/src/Pages/Admin/Courses.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import CoursesList from '../../Components/Admin/Courses/CoursesList';
+import { getCoursesApi } from '../../API/courses';
 
 const Courses = () => {
+	const [courses, setCourses] = useState([]);
+	const [reloadCourses, setReloadCourses] = useState(false);
+
+	useEffect(() => {
+		getCoursesApi().then((response) => {
+			setCourses(response.courses);
+		});
+		setReloadCourses(false);
+	}, [reloadCourses]);
+
 	return (
-		<div>
-			<h1>Estamos en Cursos...</h1>
+		<div className='courses'>
+			<CoursesList courses={courses} setReloadCourses={setReloadCourses} />
 		</div>
 	);
 };
