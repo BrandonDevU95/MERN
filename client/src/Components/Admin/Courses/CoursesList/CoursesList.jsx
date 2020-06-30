481f411d5309675ee615ff0b4d540ba05168c116
Brandon <brandon.vargas1095@hotmail.com>
2020-06-30 15:05:24 -0500

    Obtaining the Udemy API Courses Video 167

--- client/src/Components/Admin/Courses/CoursesList/CoursesList.jsx
+++ client/src/Components/Admin/Courses/CoursesList/CoursesList.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { getCourseDataUdemyApi } from '../../../../API/courses';
 
 import './CoursesList.scss';
 
 const CoursesList = (props) => {
 	const { courses, setReloadCourses } = props;
-	console.log(props);
+
+	if (courses.length > 0) {
+		courses.forEach((course) => {
+			getCourseDataUdemyApi(course.idCourse).then((response) => {
+				console.log(response);
+			});
+		});
+	}
+
 	return (
 		<div>
 			<h1>Lorem ipsum dolor sit amet.</h1>
