40a75fc75e7302160caef9dc302bffc274b62479
Brandon <brandon.vargas1095@hotmail.com>
2020-07-01 14:52:16 -0500

    Creating PresentationCourses Video 176

--- client/src/Components/Web/Courses/PresentationCourses/PresentationCourses.jsx
+++ client/src/Components/Web/Courses/PresentationCourses/PresentationCourses.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import AcademyLogo from '../../../../Assets/img/png/academy-logo.png';
 
 import './PresentationCourses.scss';
 
 const PresentationCourses = () => {
 	return (
-		<div>
-			<h1>Lorem ipsum dolor sit.</h1>
+		<div className='presentation-courses'>
+			<img src={AcademyLogo} alt='Cursos' />
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique
+				eligendi alias natus aliquam consequuntur, recusandae assumenda, at
+				dolor officiis modi dolores, nostrum doloremque. Est velit a
+				facilis, tempora et modi.
+			</p>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 		</div>
 	);
 };
