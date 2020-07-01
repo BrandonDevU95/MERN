c63d3321f359b0bff3d26dd3099cf19eb0fcf76c
Brandon <brandon.vargas1095@hotmail.com>
2020-07-01 15:39:03 -0500

    Obtaining Udemy API Information Video 178

--- client/src/Components/Web/Courses/CoursesList/CoursesList.jsx
+++ client/src/Components/Web/Courses/CoursesList/CoursesList.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Row, Col, Card, Button, Rate, notification } from 'antd';
+import { getCourseDataUdemyApi } from '../../../../API/courses';
 
 import './CoursesList.scss';
 
@@ -6,10 +8,41 @@ const CoursesList = (props) => {
 	const { courses } = props;
 
 	return (
-		<div>
-			<h1>Lorem ipsum dolor sit.</h1>
+		<div className='courses-list'>
+			<Row>
+				{courses.map((course) => (
+					<Col key={course._id} md={8} className='courses-list__course'>
+						<Course course={course} />
+					</Col>
+				))}
+			</Row>
 		</div>
 	);
 };
 
 export default CoursesList;
+
+function Course(props) {
+	const { course } = props;
+	const [courseInfo, setCourseInfo] = useState({});
+
+	useEffect(() => {
+		getCourseDataUdemyApi(course.idCourse)
+			.then((response) => {
+				if (response?.code !== 200) {
+					notification['warning']({
+						message: response.message,
+					});
+				} else {
+					setCourseInfo(response.data);
+				}
+			})
+			.catch(() => {
+				notification['error']({
+					message: 'Error del Servidor',
+				});
+			});
+	}, [course]);
+
+	return <p>{courseInfo.title}</p>;
+}
