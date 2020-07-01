1e32ab0d7887de426f7e34bd394145ec59fcac87
Brandon <brandon.vargas1095@hotmail.com>
2020-07-01 12:52:29 -0500

    Adding Add Courses Functionality Video 172

--- client/src/Components/Admin/Courses/AddEditCourseForm/AddEditCourseForm.jsx
+++ client/src/Components/Admin/Courses/AddEditCourseForm/AddEditCourseForm.jsx
@@ -1,7 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Form, Input, Button, notification } from 'antd';
-import { KeyOutlined, GiftOutlined, DollarOutlined } from '@ant-design/icons';
+import {
+	KeyOutlined,
+	GiftOutlined,
+	DollarOutlined,
+	LinkOutlined,
+} from '@ant-design/icons';
 import { getAccessTokenApi } from '../../../../API/auth';
+import { addCourseApi } from '../../../../API/courses';
 
 import './AddEditCourseForm.scss';
 
@@ -9,9 +15,46 @@ const AddEditCourseForm = (props) => {
 	const { setIsVisibleModal, setReloadCourses, course } = props;
 	const [courseData, setCourseData] = useState({});
 
+	const addCourse = (e) => {
+		if (!courseData.idCourse) {
+			notification['error']({
+				message: 'El ID es obligatorio',
+			});
+		} else {
+			const accesToken = getAccessTokenApi();
+
+			addCourseApi(accesToken, courseData)
+				.then((response) => {
+					const typeNotification =
+						response.code === 200 ? 'success' : 'warning';
+					notification[typeNotification]({
+						message: response.message,
+					});
+					setIsVisibleModal(false);
+					setReloadCourses(true);
+					setCourseData({});
+				})
+				.catch(() => {
+					notification['error']({
+						message: 'Error del servidor',
+					});
+				});
+		}
+	};
+
+	const updateCourse = (e) => {
+		console.log('Actualizando Curso');
+	};
+
 	return (
 		<div className='add-edit-course-form'>
-			<AddEditForm course={course} />
+			<AddEditForm
+				course={course}
+				addCourse={addCourse}
+				updateCourse={updateCourse}
+				setCourseData={setCourseData}
+				courseData={courseData}
+			/>
 		</div>
 	);
 };
@@ -19,35 +62,51 @@ const AddEditCourseForm = (props) => {
 export default AddEditCourseForm;
 
 function AddEditForm(props) {
-	const { course } = props;
+	const { course, addCourse, updateCourse, courseData, setCourseData } = props;
 	return (
 		<Form
 			className='form-add-edit'
-			onFinish={() => console.log('Submit....')}
+			onFinish={course ? updateCourse : addCourse}
 		>
 			<Form.Item>
 				<Input
 					prefix={<KeyOutlined />}
 					placeholder='Id del Curso'
-					// value={}
-					// onChange={}
+					value={courseData.idCourse}
+					onChange={(e) =>
+						setCourseData({ ...courseData, idCourse: e.target.value })
+					}
 					disabled={course ? true : false}
+				/>
+			</Form.Item>
+			<Form.Item>
+				<Input
+					prefix={<LinkOutlined />}
+					placeholder='URL del Curso'
+					value={courseData.link}
+					onChange={(e) =>
+						setCourseData({ ...courseData, link: e.target.value })
+					}
 				/>
 			</Form.Item>
 			<Form.Item>
 				<Input
 					prefix={<GiftOutlined />}
 					placeholder='Cupon de Descuento'
-					// value={}
-					// onChange={}
+					value={courseData.coupon}
+					onChange={(e) =>
+						setCourseData({ ...courseData, coupon: e.target.value })
+					}
 				/>
 			</Form.Item>
 			<Form.Item>
 				<Input
 					prefix={<DollarOutlined />}
 					placeholder='Precio del Curso'
-					// value={}
-					// onChange={}
+					value={courseData.price}
+					onChange={(e) =>
+						setCourseData({ ...courseData, price: e.target.value })
+					}
 				/>
 			</Form.Item>
 			<Form.Item>
