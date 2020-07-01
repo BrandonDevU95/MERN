0b23e644e6e81a2292bc0917d26142d5f7b638e1
Brandon <brandon.vargas1095@hotmail.com>
2020-07-01 15:05:46 -0500

    Obtaining the DB Courses Video 177

--- client/src/Components/Web/Courses/CoursesList/CoursesList.jsx
+++ client/src/Components/Web/Courses/CoursesList/CoursesList.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 import './CoursesList.scss';
 
-const CoursesList = () => {
+const CoursesList = (props) => {
+	const { courses } = props;
+
 	return (
 		<div>
 			<h1>Lorem ipsum dolor sit.</h1>
--- client/src/Pages/Courses.jsx
+++ client/src/Pages/Courses.jsx
@@ -5,12 +5,43 @@ import CoursesList from '../Components/Web/Courses/CoursesList';
 import { Row, Col, Spin, notification } from 'antd';
 
 const Courses = () => {
+	const [courses, setCourses] = useState(null);
+
+	useEffect(() => {
+		getCoursesApi()
+			.then((response) => {
+				if (response?.code !== 200) {
+					notification['warning']({
+						message: response.message,
+					});
+				} else {
+					setCourses(response.courses);
+				}
+			})
+			.catch(() => {
+				notification['error']({
+					message: 'Error del Servidor',
+				});
+			});
+	}, []);
+
 	return (
 		<Row>
 			<Col md={4} />
 			<Col md={16}>
 				<PresentationCourses />
-				<CoursesList />
+				{!courses ? (
+					<Spin
+						tip='Cargando Cursos'
+						style={{
+							textAlign: 'center',
+							width: '100%',
+							padding: '20px',
+						}}
+					/>
+				) : (
+					<CoursesList courses={courses} />
+				)}
 			</Col>
 			<Col md={4} />
 		</Row>
