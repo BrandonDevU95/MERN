9da2d1faf3c951632f2e9c21f1b878b3668be660
Brandon <brandon.vargas1095@hotmail.com>
2020-07-01 21:24:26 -0500

    Endpoint to create new post Video 183

--- Server/Controllers/Post.js
+++ Server/Controllers/Post.js
@@ -1,7 +1,23 @@
 const Post = require('../Models/Post');
 
 function addPost(req, res) {
-	console.log('Creando Post');
+	const body = req.body;
+	const post = new Post(body);
+
+	post.save((err, postStored) => {
+		if (err) {
+			res.status(500).send({ code: 500, message: 'Error del Servidor' });
+		} else {
+			if (!postStored) {
+				res.status(400).send({
+					code: 400,
+					message: 'No se ha podido crear el post',
+				});
+			} else {
+				res.status(200).send({ code: 200, message: 'Post Creado' });
+			}
+		}
+	});
 }
 
 module.exports = {
