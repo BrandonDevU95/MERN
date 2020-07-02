2059779bb632689b8ef15345bfbd7a7ca0daadd2
Brandon <brandon.vargas1095@hotmail.com>
2020-07-02 01:52:19 -0500

    Endpoint to Get Specific Post Video 187

--- Server/Controllers/Post.js
+++ Server/Controllers/Post.js
@@ -21,7 +21,7 @@ function addPost(req, res) {
 	});
 }
 
-function getPost(req, res) {
+function getPosts(req, res) {
 	const { page = 1, limit = 10 } = req.query;
 	const options = {
 		page,
@@ -84,9 +84,28 @@ function deletePost(req, res) {
 	});
 }
 
+function getPost(req, res) {
+	const { url } = req.params;
+	Post.findOne({ url }, (err, postStored) => {
+		if (err) {
+			res.status(500).send({ code: 500, message: 'Erro del Seridor' });
+		} else {
+			if (!postStored) {
+				res.status(404).send({
+					code: 404,
+					message: 'No se encontro el post',
+				});
+			} else {
+				res.status(200).send({ code: 200, post: postStored });
+			}
+		}
+	});
+}
+
 module.exports = {
 	addPost,
-	getPost,
+	getPosts,
 	updatePost,
 	deletePost,
+	getPost,
 };
--- Server/Routers/Post.js
+++ Server/Routers/Post.js
@@ -5,8 +5,9 @@ const md_auth = require('../Middleware/authenticated');
 const api = express.Router();
 
 api.post('/add-post', [md_auth.ensureAuth], PostController.addPost);
-api.get('/get-post', PostController.getPost);
+api.get('/get-posts', PostController.getPosts);
 api.put('/update-post/:id', [md_auth.ensureAuth], PostController.updatePost);
 api.delete('/delete-post/:id', [md_auth.ensureAuth], PostController.deletePost);
+api.get('/get-post/:url', PostController.getPost);
 
 module.exports = api;
